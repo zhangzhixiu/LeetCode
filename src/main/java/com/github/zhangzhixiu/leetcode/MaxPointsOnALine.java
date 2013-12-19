c13d4bd4e24ffaf0da731e012e981dbbeabc1c9a
zhangzhixiu <zhixiu1018@gmail.com>
2013-12-19 20:32:17 +0800

    Max Points on a Line

--- src/main/java/com/github/zhangzhixiu/leetcode/MaxPointsOnALine.java
+++ src/main/java/com/github/zhangzhixiu/leetcode/MaxPointsOnALine.java
@@ -3,6 +3,9 @@
  */
 package com.github.zhangzhixiu.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zhangzhixiu
  * 
@@ -29,6 +32,45 @@ public class MaxPointsOnALine
 
 	public int maxPoints(Point[] points)
 	{
-		return 0;
+		if(points.length==0)
+			return 0;
+		
+		Map<Double, Integer> result = new HashMap<Double, Integer>();
+		int max = 1;
+		
+		for(int i=0; i<points.length; i++)
+		{
+			result.clear();
+			int vertical = 1;
+			int duplicate = 1;
+			
+			for(int j=0; j<points.length; j++)
+			{
+				if(i==j) continue;
+				if (points[j].x == points[i].x && points[j].y == points[i].y)
+				{
+					duplicate++;
+					continue;
+				}
+				if((points[j].x - points[i].x) == 0)
+				{
+					vertical++;
+				}
+				else
+				{
+					Double key =  (0d + points[j].y - points[i].y) / (points[j].x - points[i].x);
+					result.put(key, result.get(key)==null?1:result.get(key)+1);
+				}
+			}
+			
+			for(Integer value: result.values())
+			{
+				max = value+duplicate>max?value+duplicate:max;
+			}
+			max = vertical>max?vertical:max;
+			max = duplicate>max?duplicate:max;
+		}
+		
+		return max;
 	}
 }
--- src/test/java/com/github/zhangzhixiu/leetcode/MaxPointsOnALineTest.java
+++ src/test/java/com/github/zhangzhixiu/leetcode/MaxPointsOnALineTest.java
@@ -3,12 +3,72 @@
  */
 package com.github.zhangzhixiu.leetcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * http://oj.leetcode.com/problems/max-points-on-a-line/
  * @author zhangzhixiu
  *
  */
+@RunWith(JUnit4.class)
 public class MaxPointsOnALineTest
 {
-
+	private MaxPointsOnALine maxPointsOnALine;
+	
+	@Before
+	public void init()
+	{
+		maxPointsOnALine = new MaxPointsOnALine();
+	}
+	
+	@Test
+	public void test1()
+	{
+		MaxPointsOnALine.Point[] p = {};
+		Assert.assertEquals(0, maxPointsOnALine.maxPoints(p));
+	}
+	
+	@Test
+	public void test2()
+	{
+		MaxPointsOnALine.Point p1 = maxPointsOnALine.new Point(1,1);
+		MaxPointsOnALine.Point p2 = maxPointsOnALine.new Point(1,1);
+		MaxPointsOnALine.Point p3 = maxPointsOnALine.new Point(1,1);
+		MaxPointsOnALine.Point[] p = {p1,p2,p3};
+		Assert.assertEquals(3, maxPointsOnALine.maxPoints(p));
+	}
+	
+	@Test
+	public void test3()
+	{
+		MaxPointsOnALine.Point p1 = maxPointsOnALine.new Point(1,1);
+		MaxPointsOnALine.Point p2 = maxPointsOnALine.new Point(1,2);
+		MaxPointsOnALine.Point p3 = maxPointsOnALine.new Point(1,3);
+		MaxPointsOnALine.Point[] p = {p1,p2,p3};
+		Assert.assertEquals(3, maxPointsOnALine.maxPoints(p));
+	}
+	
+	@Test
+	public void test4()
+	{
+		MaxPointsOnALine.Point p1 = maxPointsOnALine.new Point(1,1);
+		MaxPointsOnALine.Point p2 = maxPointsOnALine.new Point(2,2);
+		MaxPointsOnALine.Point p3 = maxPointsOnALine.new Point(3,3);
+		MaxPointsOnALine.Point p4 = maxPointsOnALine.new Point(3,4);
+		MaxPointsOnALine.Point[] p = {p1,p2,p3,p4};
+		Assert.assertEquals(3, maxPointsOnALine.maxPoints(p));
+	}
+	
+	@Test
+	public void test5()
+	{
+		MaxPointsOnALine.Point p1 = maxPointsOnALine.new Point(0,0);
+		MaxPointsOnALine.Point p2 = maxPointsOnALine.new Point(1,0);
+		MaxPointsOnALine.Point[] p = {p1,p2};
+		Assert.assertEquals(2, maxPointsOnALine.maxPoints(p));
+	}
 }
